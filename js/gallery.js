f0ae88d41f489fac1feaa541104f9dd631d81c30
Zoriana-Burian <ke7sha1991@gmail.com>
2020-10-01 15:33:05 +0300

    довиконання галереї

--- js/gallery.js
+++ js/gallery.js
@@ -2,11 +2,17 @@
 
 import images from './gallery-items.js';
 
+const refs = {
+  gallery: document.querySelector('.js-gallery'),
+  lightbox: document.querySelector('.js-lightbox'),
+  lightboxOverlay: document.querySelector('.lightbox__overlay'),
+  lightboxImage: document.querySelector('.lightbox__image'),
+  closeBtn: document.querySelector('button[data-action="close-lightbox"]')
+}
 
-const paletteImages = document.querySelector('.js-gallery');
 const imagesMarkup = createGalleryElements(images);
 
-paletteImages.insertAdjacentHTML('beforeend', imagesMarkup);
+refs.gallery.insertAdjacentHTML('beforeend', imagesMarkup);
 
 function createGalleryElements(item) {
     return item
@@ -28,6 +34,49 @@ function createGalleryElements(item) {
   .join('');
 }
 
+refs.gallery.addEventListener('click', onGalleryClick);
+refs.closeBtn.addEventListener('click', onCloseModalClickBtn);
+refs.lightboxOverlay.addEventListener('click', OnCloseModalOnClick);
 
 
+function onGalleryClick(evt) {
+  evt.preventDefault();
+  if(!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  onOpenModalClickImg(evt);
+  
+}
+
+function onOpenModalClickImg(evt) {
+  refs.lightbox.classList.add('is-open');
+  refs.lightboxImage.src = evt.target.dataset.source;
+  refs.lightboxImage.alt = evt.target.alt;
+
+  window.addEventListener('keydown', onCloseModalEscKey);
+}
+
+
+function onCloseModalClickBtn() {
+  refs. lightbox.classList.remove('is-open');
+  refs.lightboxImage.src = '';
+  refs.lightboxImage.alt = '';
+
+  window.removeEventListener('keydown', onCloseModalEscKey);
+}
+
+function onCloseModalEscKey(evt) {
+  if(evt.code === 'Escape') {
+    onCloseModalClickBtn();
+  }
+  
+}
+
+function OnCloseModalOnClick(evt){
+  if(evt.target === evt.currentTarget){
+    onCloseModalClickBtn();
+
+  }
+}
 
